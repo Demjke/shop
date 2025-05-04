d777ea10a1d7bb9e308e13b4b5a5e7e76a4eaa84
Vitaliy Antipov <demjke64@gmail.com>
2025-05-04 17:02:47 +0400

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App';
 import "./assets/styles/main.scss";
 import store from './redux/store';
@@ -9,9 +9,9 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/shop">
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 
 );
