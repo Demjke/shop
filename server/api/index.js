9ad79cbec77d12af61f88a536b48d98b1ea03be1
Vitaliy Antipov <demjke64@gmail.com>
2025-05-04 11:28:19 +0400

    update

---
+++ server/api/index.js
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express from "express";
+import categoriesRoutes from '../routes/categories.js';
+import productsRoutes from '../routes/products.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/products', productsRoutes);
+app.use('/api/categories', categoriesRoutes)
+
+// Раздача статических файлов из папки uploads
+app.use("/uploads", express.static("./public"));
+
+export default app;
