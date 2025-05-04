d13ba6484901055cdf5318f74177807c1b9e1a80
Vitaliy Antipov <demjke64@gmail.com>
2025-05-04 15:08:24 +0400

    update

--- server/api/index.js
+++ server/api/index.js
@@ -1,55 +1,18 @@
 import cors from "cors";
 import express from "express";
-import categoriesRoutes from "../routes/categories.js";
-import productsRoutes from "../routes/products.js";
+import categoriesRoutes from '../routes/categories.js';
+import productsRoutes from '../routes/products.js';
 
 const app = express();
 
-// Настройка CORS
 app.use(cors({
-  origin: "https://demjke.github.io",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+  origin: 'https://demjke.github.io'
 }));
-
-// Явная обработка OPTIONS для всех маршрутов
-app.options("*", (req, res) => {
-  res.set({
-    "Access-Control-Allow-Origin": "https://demjke.github.io",
-    "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type,Authorization",
-    "Access-Control-Allow-Credentials": "true",
-  });
-  res.status(204).send();
-});
-
-// Логирование всех запросов
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  console.log("Headers:", req.headers);
-  res.on("finish", () => {
-    console.log(`Response Status: ${res.statusCode}`);
-    console.log("Response Headers:", res.getHeaders());
-  });
-  next();
-});
-
 app.use(express.json());
+app.use('/api/products', productsRoutes);
+app.use('/api/categories', categoriesRoutes)
 
-// Маршруты API
-app.use("/api/categories", categoriesRoutes);
-app.use("/api/products", productsRoutes);
-
-// Тестовый эндпоинт
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API is working" });
-});
-
-// Обработка ошибок
-app.use((err, req, res, next) => {
-  console.error("Server error:", err);
-  res.status(500).json({ error: "Internal server error" });
-});
+// Раздача статических файлов из папки uploads
+app.use("/uploads", express.static("./uploads"));
 
 export default app;
