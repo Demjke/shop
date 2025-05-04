770f1aca0617a6613794924e6ded7e9024b001e7
Vitaliy Antipov <demjke64@gmail.com>
2025-05-04 11:55:49 +0400

    update

--- server/api/index.js
+++ server/api/index.js
@@ -1,21 +1,24 @@
-// api/index.js
 import cors from "cors";
 import express from "express";
-import serverless from 'serverless-http';
-import categoriesRoutes from '../routes/categories.js';
-import productsRoutes from '../routes/products.js';
+import serverless from "serverless-http";
+import categoriesRoutes from "../routes/categories.js";
+import productsRoutes from "../routes/products.js";
 
 const app = express();
 
+// CORS настройка
 app.use(cors({
-  origin: 'https://demjke.github.io',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: "https://demjke.github.io", // или ['https://site1', 'https://site2']
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
 app.use(express.json());
-app.use('/api/products', productsRoutes);
-app.use('/api/categories', categoriesRoutes);
 
+// Роуты
+app.use("/api/categories", categoriesRoutes);
+app.use("/api/products", productsRoutes);
+
+// Экспорт как serverless handler
 export const handler = serverless(app);
